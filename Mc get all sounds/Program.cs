678b9db6306746353748cedfcf14196f21c00d5f
TheAwesomeGoat <roelofrijken9@gmail.com>
2021-07-04 17:56:27 +0200

    Mc Bong

--- Mc get all sounds/Program.cs
+++ Mc get all sounds/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Mc_get_all_sounds
@@ -12,7 +14,7 @@ namespace Mc_get_all_sounds
     {
         struct SoundData
         {
-            public string Filename;
+            public string SoundPath;
             public string Hash;
             public int Size;
         }
@@ -25,18 +27,16 @@ namespace Mc_get_all_sounds
             Console.CursorVisible = false;
 
             string[] Versions = Directory.GetFiles($@"{Appdata}\.minecraft\assets\indexes\");
-
-            int Index = 0;
-            int LastIndex = 0;
-
             foreach (var item in Versions)
                 Console.WriteLine($"[   ] - {item}");
 
-            Console.SetCursorPosition(2, 0);
-            Console.Write("#");
-
+            int Index = 0;
+            int LastIndex = 0;
             while (true)
             {
+                Console.SetCursorPosition(2, Index);
+                Console.Write("#");
+
                 ConsoleKey key = Console.ReadKey().Key;
                 switch (key)
                 {
@@ -58,15 +58,12 @@ namespace Mc_get_all_sounds
                 if (Index < 0)
                     Index = Versions.Count() - 1;
 
-                Console.SetCursorPosition(2, Index);
-                Console.Write("#");
                 Console.SetCursorPosition(2, LastIndex);
                 Console.Write(" ");
 
                 LastIndex = Index;
             }
         }
-
         static void Extract(string Version)
         {
             string VersionFolder = Path.GetFileName(Version).Replace(".json", "");
@@ -85,17 +82,21 @@ namespace Mc_get_all_sounds
                     dynamic SoundItem = item.First;
                     soundDataList.Add(new SoundData
                     {
-                        Filename = path,
+                        SoundPath = path,
                         Hash = SoundItem.hash,
                         Size = SoundItem.size
                     });
                     Console.WriteLine(path);
                 }
             }
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
             string SoundObject = "";
             foreach (var soundData in soundDataList)
             {
-                Console.Write($"Writing Out: {soundData.Filename} - {soundData.Size}kb ");
+                Console.Write($"Writing Out: {soundData.SoundPath} - {soundData.Size}kb, ");
                 foreach (var item in SoundFiles)
                 {
                     if (item.Contains(soundData.Hash))
@@ -106,9 +107,7 @@ namespace Mc_get_all_sounds
                 }
                 if (File.Exists(SoundObject))
                 {
-                    byte[] soundBytes = File.ReadAllBytes(SoundObject);
-
-                    string[] split = soundData.Filename.Split('/');
+                    string[] split = soundData.SoundPath.Split('/');
 
                     string Filen = split.Last();
                     string pth = string.Join(@"\", split.Take(split.Count() - 1));
@@ -117,21 +116,28 @@ namespace Mc_get_all_sounds
                     bool exists = File.Exists($@"{VersionFolder}\{pth}\{Filen}");
                     if (!exists)
                     {
-                        File.WriteAllBytes($@"{VersionFolder}\{pth}\{Filen}", soundBytes);
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("Done.");
-                        Console.ForegroundColor = DefaultC;
+                        File.Copy(SoundObject, $@"{VersionFolder}\{pth}\{Filen}");
+                        PrintColor("Done.", ConsoleColor.Green);
                     }
                     else
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine("Exists.");
-                        Console.ForegroundColor = DefaultC;
-                    }
+                        PrintColor("Exists.", ConsoleColor.DarkYellow);
                 }
+                else
+                    PrintColor("Hash Not Found.", ConsoleColor.Red);
+
             }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
             Console.WriteLine("\nDone.");
+            Console.WriteLine($"Elapsed Time: {elapsedTime}");
             Console.ReadKey();
+        }
+        static void PrintColor(string msg, ConsoleColor col)
+        {
+            Console.ForegroundColor = col;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = DefaultC;
         }
     }
 }
